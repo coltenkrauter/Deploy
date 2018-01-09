b16cb1831c5643432779b95a561d7b036d7ccf44
Colten Krauter <coltenkrauter@gmail.com>
2018-01-09 13:21:33 -0800

    Update

--- endpoints/deploy.py
+++ endpoints/deploy.py
@@ -8,15 +8,10 @@ import subprocess
 import os
 
 def verify_hmac_hash(data, signature):
-    data_to_sign = data.decode('utf-8')
-    logger.log()
-
-    signature_to_sign = signature.encode('utf-8')
-    mac = hmac.new(config.GITHUB_SECRET, data_to_sign, hashlib.sha1)
-    logger.log('sha1=' + mac.hexdigest())
-
-    return hmac.compare_digest('sha1=' + mac.hexdigest(), signature_to_sign)
-
+    github_secret = bytes(config.GITHUB_SECRET)
+    mac = hmac.new(github_secret, msg=bytes(data), digestmod=hashlib.sha1)
+    return hmac.compare_digest('sha1=' + mac.hexdigest(), signature)
+        
 @app.route("/payload/", methods=['POST'])
 def github_payload():
     try:
