5d6fbf3f5871e50efb0c92157db3b64f62eb2109
Colten Krauter <coltenkrauter@gmail.com>
2018-01-09 12:34:35 -0800

    Debug

--- endpoints/deploy.py
+++ endpoints/deploy.py
@@ -17,29 +17,4 @@ def verify_hmac_hash(data, signature):
 
 @app.route("/payload/", methods=['POST'])
 def github_payload():
-    try:
-        signature = request.headers.get('X-Hub-Signature')
-        data = request.data
-        if verify_hmac_hash(data, signature):
-            if request.headers.get('X-GitHub-Event') == "ping":
-                return jsonify({'msg': 'Ok'})
-            if request.headers.get('X-GitHub-Event') == "push":
-                payload = request.get_json()
-                if payload['commits'][0]['distinct'] == True:
-                    try:
-                        cmd_output = subprocess.check_output(
-                            ['git', 'pull', 'origin', 'master'],)
-                        
-                        return jsonify({'msg': str(cmd_output)})
-                    except subprocess.CalledProcessError as error:
-        
-                        return jsonify({'msg': str(error.output)})
-                else:
-                    return jsonify({'msg': 'nothing to commit'})
-
-        else:
-            return jsonify({'msg': 'invalid hash'})
-    except Exception as error:
-        logger.log()
-        response, status = responder.response(code=401, message='Unable to verify secret key.')
-        return jsonify(response), status
+    return jsonify({'msg': 'invalid hash'})
