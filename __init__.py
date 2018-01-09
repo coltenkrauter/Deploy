ab873d65b429e0b664dcd755f21bc1c1bfee66e5
Colten Krauter <coltenkrauter@gmail.com>
2018-01-09 12:38:46 -0800

    Update

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from Deploy.util import logger
 
-app = Flask(__name__)
+try:
+    app = Flask(__name__)
 
-# Allow CORS (Cross Origin Resource Sharing) in order to for swagger doc to work
-CORS(app)
+    # Allow CORS (Cross Origin Resource Sharing) in order to for swagger doc to work
+    CORS(app)
 
-from . import endpoints
+    from . import endpoints
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+    if __name__ == '__main__':
+    	app.run(host='0.0.0.0', debug=True)
+except:
+    logger.log
