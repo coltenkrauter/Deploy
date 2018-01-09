6abef90899270bcff745dbf7852baea45b0f6228
Colten Krauter <coltenkrauter@gmail.com>
2018-01-09 11:11:49 -0800

    Revert "Debug"
    This reverts commit a2e9b4ff241a7ebe26a715f6e0780593ddad38be.

--- logic/deploy.py
+++ logic/deploy.py
@@ -2,13 +2,13 @@ from Deploy.util import logger
 import git 
 
 def deploy(data, path):
-    logger.log('Recieved payload')
-    logger.log('Path: ' + path)
+    logger.deploy('Recieved payload')
+    logger.deploy('Path: ' + path)
     
     g = git.cmd.Git("/home4/specica9/public_html/" + path)
     g.pull()
 
     message = "HTTP 200, OK: Success!"
-    logger.log(message)
+    logger.deploy(message)
 
     return message, 200
