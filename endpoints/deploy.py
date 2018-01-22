e75085e9efc76280c59e6649c4902a8ccc1bc8c0
Colten Krauter <coltenkrauter@gmail.com>
2018-01-21 19:50:21 -0800

    Decode cmd_output

--- endpoints/deploy.py
+++ endpoints/deploy.py
@@ -26,7 +26,7 @@ def github_payload():
                 # Check if there are any commits to pull
                 if payload['commits'][0]['distinct'] == True:
                     try:
-                        cmd_output = subprocess.check_output(['git', 'pull', 'origin', 'master'], cwd="../" + request.args.get('folder'))
+                        cmd_output = subprocess.check_output(['git', 'pull', 'origin', 'master'], cwd="../" + request.args.get('folder')).decode("utf-8") 
                         slack.log(cmd_output)
                         return jsonify({'msg': str(cmd_output)})
                     except subprocess.CalledProcessError as error:
