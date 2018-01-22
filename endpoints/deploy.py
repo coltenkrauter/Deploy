7dc92a2296f4c0155616521ebffc89df15066dcb
Colten Krauter <coltenkrauter@gmail.com>
2018-01-21 19:17:20 -0800

    Adding subprocess command

--- endpoints/deploy.py
+++ endpoints/deploy.py
@@ -5,7 +5,6 @@ from Deploy.config import config
 import hmac
 import hashlib
 import subprocess
-import os
 
 def verify_hmac_hash(data, signature):
     github_secret = config.GITHUB_SECRET
@@ -23,11 +22,12 @@ def github_payload():
                 return jsonify({'msg': 'Ok'})
             if request.headers.get('X-GitHub-Event') == "push":
                 payload = request.get_json()
+
+                # Check if there are any commits to pull
                 if payload['commits'][0]['distinct'] == True:
                     try:
-                        cmd_output = subprocess.check_output(
-                            ['git', 'pull', 'origin', 'master'],)
-                        
+                        cmd_output = subprocess.check_output(['git', 'pull', 'origin', 'master'], cwd="../../")
+                        slack.log(cmd_output)
                         return jsonify({'msg': str(cmd_output)})
                     except subprocess.CalledProcessError as error:
         
@@ -40,6 +40,6 @@ def github_payload():
             response, status = responder.response(code=401, message='Unable to verify secret key.')
             return jsonify(response), status
     except Exception as error:
-        logger.log()
+        slack.log()
         response, status = responder.response(code=500, message='Internal error.')
         return jsonify(response), status
