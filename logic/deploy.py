0892251fc1aafddabbc937191512db9c1196cc4e
Colten Krauter <coltenkrauter@gmail.com>
2018-01-22 16:11:59 -0800

    Update slack messages

--- logic/deploy.py
+++ logic/deploy.py
@@ -12,10 +12,21 @@ import subprocess
 def pull(request):
     payload = request.get_json()
 
+    username = ''
+
+    if not payload['repository'] or not payload['repository']['name']:
+        return {'msg': 'Repository name missing'}
+    if payload['committer'] or payload['committer']['username']:
+        username = 'Committed by '+payload['committer']['username']+'\n'
+    
+    repository = payload['repository']['name']
+
+    slack.log(username + 'Repository: '+repository)
+    
     # Check if there are any commits to pull
     if len(payload['commits']) > 0 and payload['commits'][0]['distinct'] == True:
         try:
-            cmd_output = subprocess.check_output(['git', 'pull', 'origin', 'master'], cwd="../" + request.args.get('folder')).decode("utf-8") 
+            cmd_output = subprocess.check_output(['git', 'pull', 'origin', 'master'], cwd="../" + repository).decode("utf-8") 
             slack.log(cmd_output)
             return {'msg': str(cmd_output)}
 
