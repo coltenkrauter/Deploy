60e1305ca69de76905642060bfbae59823a17593
Colten Krauter <coltenkrauter@gmail.com>
2018-04-26 10:43:47 -0700

    Update slack logger

--- util/slack.py
+++ util/slack.py
@@ -1,16 +1,37 @@
 # Thanks tomdaley92 for the simple slack logging solution with traceback
 # Credit: https://github.com/tomdaley92
 
-import requests
-import traceback
 from codepuller import config
+import requests
+import traceback as error
 
+ENVIRONMENT = "`Colten PC - DEV`\n\n\n\n"
+config.SLACK_WEBHOOK_LOGGER = "https://hooks.slack.com/services/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"
 
-def log(message = None):
-    data = { 'text' : '```'+traceback.format_exc()+'```' }
+def log(text=None, traceback=True):
+    data = {
+        "text":"",
+        "traceback":"",
+        "newlines":""
+    }
 
-    if message:
-        data = { 'text' : '```'+str(message)+'```' }
-        
-    requests.post(config.SLACK_WEBHOOK_DEPLOY, json=data)
+    if text is not None:
+        data["text"] = "*Message:* "+str(text)
+
+    if traceback:
+        data["traceback"] = " ```"+str(error.format_exc())+"``` "
     
+    if data["text"] and data["traceback"]:
+        data["newlines"] = "\n\n"
+
+    slack = {
+        "text":ENVIRONMENT+" "+data["text"]+data["newlines"]+data["traceback"]
+    }
+    # Post to Slack
+    r = requests.post(config.SLACK_WEBHOOK_LOGGER, json=slack)
+    
+    # Print to standard out
+    print(text)
+
+if __name__ == '__main__':
+    log("Testing functionality of Slack logger.", False)
